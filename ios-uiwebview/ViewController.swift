d315996fb683297ccb67a75665a1d0eaca30f808
hawkup <spiritsquare@gmail.com>
2017-11-05 23:59:06 +0700

    Added webview

--- ios-uiwebview/ViewController.swift
+++ ios-uiwebview/ViewController.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = URL (string: "https://www.google.com");
+        webView.load(URLRequest(url: url!))
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
